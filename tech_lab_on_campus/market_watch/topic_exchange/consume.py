2832efde75d4fdba77c12df00adf32795a45df63
Max Raffel <44725991+maxraffel@users.noreply.github.com>
2026-03-12 01:46:33 +0000

    topic exchange!

--- tech_lab_on_campus/market_watch/topic_exchange/consume.py
+++ tech_lab_on_campus/market_watch/topic_exchange/consume.py
@@ -23,6 +23,8 @@ def main(sector: str, queueName: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
+    bindingKey = f"#.{sector}"
+
     
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
@@ -35,5 +37,8 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+
+    sector = sys.argv[1]
+    queue = sys.argv[2]
 
     sys.exit(main(sector,queue))
